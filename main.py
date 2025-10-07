8deeb4c0ade407fa3cd7cbf62d5c12ce3e5d0c83
divakaruthar <divakaruthar@gmail.com>
2025-10-07 18:25:31 +0530

    CRUD APIs

--- main.py
+++ main.py
@@ -10,10 +10,41 @@ def greet():
 
 # Create a object using constructor
 products = [
-    Product(1,"phone","budget phone", 99, 10),
-    Product(2,"laptop","gaming laptop", 999, 6)
+    Product(id=1, name="Phone", description="A smartphone", price=699.99, quantity=50),
+    Product(id=2, name="Laptop", description="A powerful laptop", price=999.99, quantity=30),
+    Product(id=3, name="Pen", description="A blue ink pen", price=1.99, quantity=100),
+    Product(id=4, name="Table", description="A wooden table", price=199.99, quantity=20),
 ]
 
 @app.get("/products")
 def get_all_products():
     return products
+
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+    return "Product Not Found"
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return product
+
+
+@app.put("/product")
+def update_product(id: int, product: Product):
+    for i in range(len(products)):
+        if products[i].id == id:
+            products[i] = product
+            return "Product Updated Sucessfully"
+    return "No Product Found"
+
+@app.delete("/product")
+def delete_product(id: int):
+    for i in range(len(products)):
+        if products[i].id == id:
+            del products[i]
+            return "Product Delete Successfully"
+    return "Product Not Found"
